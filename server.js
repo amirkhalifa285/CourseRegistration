740f01b5269553527bc3258f9fb3a4ac34045865
amirkhalifa285 <amir.kh28@hotmail.com>
2024-12-27 18:30:12 +0200

    connect database

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-require
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('./config/db');
+const morgan = require('morgan');
+
+const authRoutes = require('./routes/authRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/courses', courseRoutes);
+
+
+const { errorHandler } = require('./middlewares/errorHandler');
+app.use(errorHandler);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
